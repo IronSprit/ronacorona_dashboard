9cbb1c69d92bfc27ed45be41d1e80ac84dcefdda
IronSprit <biagraathict@naver.com>
2023-01-04 18:04:49 +0900

    update

---
+++ main.py
@@ -0,0 +1,13 @@
+# Run this app with `python app.py` and
+# visit http://127.0.0.1:8050/ in your web browser.
+
+from dash import Dash, html, dcc
+import plotly.express as px
+import pandas as pd
+
+app = Dash(__name__)
+
+app.layout = html.Div(html.H1("Hello"), style={""})
+
+if __name__ == '__main__':
+    app.run_server(host="192.168.83.147", debug=True)
